791c7b61a3429376759ee678642aa93a6f518956
Manel Rezki <96089330+manelmanelmanelmanel@users.noreply.github.com>
2022-12-06 17:06:21 +0100

    Ajout bonus à tous les étudiants 13

--- src/EtudiantService.java
+++ src/EtudiantService.java
@@ -1,88 +1,73 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-
 public class EtudiantService {
 
-
-
-		private IEtudiant StudRep;
-		private IUniversite UnivRep;
-		private IJournal j;
-
-		
-		public void setEtudRep(IEtudiant StudRep) {
-			this.StudRep = StudRep;
-		}
-@@ -23,35 +18,20 @@ public void setJournal(IJournal j) {
-		public IJournal getJournal() {
-			return this.j;
-		}
+@@ -21,17 +24,15 @@ public IJournal getJournal() {
 		public IEtudiant getStudRep() {
 			return this.StudRep;
 		}
 
+		
 	boolean inscription (int matricule, String nom, String prénom, String email,String pwd, int id_universite) throws SQLException	
 	{
 
-
+		
 	    Etudiant stud = new Etudiant(matricule, nom, prénom, email,pwd,id_universite);
-	    Universite univ= UnivRep.GetById(id_universite,j);
 	    Universite univ= UnivRep.GetById(id_universite, j);
 
+	    
 	    j.outPut_Msg("Log: début de l'opération d'ajout de l'étudiant avec matricule "+matricule);
 
-	    if(email == null || email.length() == 0)
-	    {
-	    	return false;
-	    }
-
-
-
-
-	    if (StudRep.Exists(matricule,j))
-	    {
-	        return false;
-	    }
-
-		if (StudRep.Exists(email, j))
-	    {
-	        return false;
-	    }
 
 
 
 		 if (univ.getPack() == TypePackage.Standard)
 	     {
 	          stud.setNbLivreMensuel_Autorise(10);
-@@ -61,24 +41,24 @@ else if (univ.getPack() == TypePackage.Premium)
+@@ -40,25 +41,34 @@ else if (univ.getPack() == TypePackage.Premium)
+	     {
 	    	 stud.setNbLivreMensuel_Autorise(10*2);
 	     }                           
 
-
+	     
 		 StudRep.add(stud, j);
 
+		 
 		 j.outPut_Msg("Log: Fin de l'opération d'ajout de l'étudiant avec matricule "+matricule);
 		 return true;
 
 
-		
+
+	}
+
+
+
+
+	public void ajouterBonus() throws SQLException {
+		ArrayList<Etudiant> etudiants = StudRep.getEtudiants();
+
+		for(Etudiant e : etudiants) {
+			Universite univ = UnivRep.GetById(e.getId_universite(), new ScreenJourn());
+			e.giveBonus(univ);
+		}
 	}
 
 
 
 public ArrayList<Etudiant> GetEtudiantParUniversitye()
 {
-    //...
 
+    //...
 	return new ArrayList<>(4);
 }
 public ArrayList<Etudiant> GetEtudiatparLivreEmprunte()
 {
-    //...
 
+    //...
 	return new ArrayList<>(4);
 
 }

020d363c9068cd81b0737250bfd66d3f49ba7b2b
Manel Rezki <96089330+manelmanelmanelmanel@users.noreply.github.com>
2022-12-06 17:16:08 +0100

    _

--- IUniversite.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-import java.sql.SQLException;
-
-import Journal.IJournal;
-import Services.Universite;
-
-public interface IUniversite {
-	public Universite GetById(int universityId,  IJournal j) throws SQLException;
-}

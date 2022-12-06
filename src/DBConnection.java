73538e41db7500232c7adb190dcad38e3a050a5b
Manel Rezki <96089330+manelmanelmanelmanel@users.noreply.github.com>
2022-12-06 16:48:45 +0100

    Singleton

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,32 +1,37 @@
+package BD;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	   
-		String BDD = "nomBD";
+
+		String BDD = "gestion";
 		String url = "jdbc:mysql://localhost:3306/" + BDD;
 		String user = "root";
 		String passwd = "";
 	    private Connection conn;
+	    private static DBConnection instance;
 
-	   private static DBConnection instance;
-	   private DBConnection(){};
-	   public static DBConnection get instance()
-	   {
-		if(instance==null) instance=new DBConnection();
-		return instance;
-	   }
-	    public DBConnection() throws SQLException {
-			conn=DriverManager.getConnection(url, user,passwd);
+
+			
+	    private DBConnection() throws SQLException {
+			conn = DriverManager.getConnection(url, user,passwd);
+
 		}
 
-	    
+
 	    public Connection getConn() {
 			return conn;
 		}
 
+	    public static DBConnection getInstance() throws SQLException {
+	    	if(instance == null) {
+	    		instance = new DBConnection();
+	    	}
+	    	return instance;
+	    }
 
-		
+
+
 	
 }
